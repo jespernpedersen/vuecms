db91432cccee5e7e5702b33d9f9422263159e8bb
Jesper Pedersen <jeppenp12@gmail.com>
2020-03-31 02:57:45 +0200

    Install Vue Unicons for frontend

--- cms/src/main.js
+++ cms/src/main.js
@@ -3,9 +3,15 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
+import Unicon from 'vue-unicons'
+import { uniLayerGroupMonochrome, uniCarWash } from 'vue-unicons/src/icons'
+
+Unicon.add([uniLayerGroupMonochrome, uniCarWash])
+Vue.use(Unicon)
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
